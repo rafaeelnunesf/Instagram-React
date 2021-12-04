791a90840167f95add7a789bc633356c15450e16
rafaeelnunes <rafaelnfsq@gmail.com>
2021-12-04 11:01:14 -0300

    posts finalizado

--- src/Posts.js
+++ src/Posts.js
@@ -1,54 +1,36 @@
 export default function Posts() {
+    const infoPosts = [
+        {nomePerfil:"meowed", fotoPerfil:"Assets/meowed 2.png", fotoPost: "Assets/gato-telefone 1.png", fotoCurtidor:"Assets/respondeai 2.png",nomeCurtidor:"respondeai"  },
+        {nomePerfil:"barked", fotoPerfil:"Assets/barked 2.png" , fotoPost: "Assets/dog 1.png", fotoCurtidor:"Assets/adorableanimals 2.png",nomeCurtidor:"adorable_animals" }
+
+    ]
     return(
         <div class="posts">
-          <div class="post">
-            <div class="header-post">
-              <img src="Assets/meowed 2.png" />
-              <h2>meowed</h2>
-              <h3>...</h3>
-            </div>
-            <img src="Assets/gato-telefone 1.png" />
-            <div class="reactions">
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-              <ion-icon
-                class="bookmark-reaction"
-                name="bookmark-outline"
-              ></ion-icon>
-            </div>
-            <div class="footer-post">
-              <img src="Assets/respondeai 2.png" />
-              <h4>
-                Curtido por <span>respondeai</span> e
-                <span>outras 101.523 pessoas</span>
-              </h4>
-            </div>
-          </div>
-          <div class="post">
-            <div class="header-post">
-              <img src="Assets/barked 2.png" />
-              <h2>barked</h2>
-              <h3>...</h3>
-            </div>
-            <img src="Assets/dog 1.png" />
-            <div class="reactions">
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-              <ion-icon
-                class="bookmark-reaction"
-                name="bookmark-outline"
-              ></ion-icon>
-            </div>
-            <div class="footer-post">
-              <img src="Assets/adorableanimals 2.png" />
-              <h4>
-                Curtido por <span>adorable_animals</span> e
-                <span>outras 99.159 pessoas</span>
-              </h4>
-            </div>
-          </div>
+            {infoPosts.map(({nomePerfil,fotoPerfil,fotoPost,fotoCurtidor,nomeCurtidor})=><Post nomePerfil={nomePerfil} fotoPerfil={fotoPerfil} fotoPost = {fotoPost} fotoCurtidor = {fotoCurtidor} nomeCurtidor={nomeCurtidor}/>)}
         </div>
+    )
+}
+function Post({nomePerfil,fotoPerfil,fotoPost,fotoCurtidor,nomeCurtidor}){
+    const iconNames = ["heart-outline","chatbubble-outline","paper-plane-outline","bookmark-outline"]
+
+    return(
+    <div class="post">
+        <div class="header-post">
+          <img src={fotoPerfil} />
+          <h2>{nomePerfil}</h2>
+          <h3>...</h3>
+        </div>
+        <img src={fotoPost} />
+        <div class="reactions">
+            {iconNames.map((item)=> <ion-icon name={item}></ion-icon>)}
+        </div>
+        <div class="footer-post">
+          <img src={fotoCurtidor} />
+          <h4>
+            Curtido por <span>{nomeCurtidor}</span> e
+            <span>outras 101.523 pessoas</span>
+          </h4>
+        </div>
+      </div>
     )
 }
