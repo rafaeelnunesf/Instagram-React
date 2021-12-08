76226a72f3c0c4d9722cc64eb2ad69e6ba4a299d
Guga <contato.gustavobarbosa@outlook.com>
2021-12-08 15:24:09 -0300

    Preparando revisão de código

--- src/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    const iconNames = ["home","search-outline","add-circle-outline","heart-outline","person-outline"]
-    return(
-        <footer>
-            {iconNames.map((item)=> <ion-icon class="icon-footer" name={item}></ion-icon>)}
-        </footer>
-    )
-}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import App from "./App";
-import ReactDOM from 'react-dom';
-
-const teste = document.querySelector(".root")
-ReactDOM.render(App(),teste);
